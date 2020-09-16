9ff821724721dacf6c30098fe4868ce70385b28e
Riko Hermansyah <mr.rikohermansyah@gmail.com>
2020-09-16 13:32:50 +0700

    Creation

---
+++ src/components/pages/HomePage/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import HeroSection from '../../HeroSection'
+import Pricing from '../../Pricing'
+import {homeObjOne, homeObjTwo, homeObjThree, homeObjFour} from "./Data"
+
+function Home() {
+    return (
+        <>
+        <HeroSection {...homeObjOne} />
+        <Pricing />
+        <HeroSection {...homeObjThree} />
+        <HeroSection {...homeObjTwo} />
+        <Pricing />
+        <HeroSection {...homeObjFour} />
+            
+        </>
+    );
+}
+
+export default Home
